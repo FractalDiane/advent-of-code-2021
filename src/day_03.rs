f1993fd84d8c61cf5b0cebb34cf881f608366b35
Diane Sparks <duncan.sprks@gmail.com>
2021-12-03 20:56:05 -0800

    Day 3 part 2

--- src/day_03.rs
+++ src/day_03.rs
@@ -24,19 +24,56 @@ pub fn day_03(file: &str) -> u32 {
 	let len = vec.len();
 	let gamma = (0..size).fold(0, |acc, i| acc | ((counts[i] > (len / 2)) as u32) << (size - i - 1));
 
-	// I am ONLY doing this to ensure compatibility with >8 bit numbers
+	// I am ONLY using u32s and by extension doing this to ensure compatibility with >8 bit numbers
 	gamma * (!gamma & (1 << size) - 1)
+}
+
+fn trim_rating(mut vec: Vec<String>, size: usize, mut counts: Vec<usize>, co2: bool) -> u32 {
+	let mut i = size - 1;
+	let mut len = vec.len();
+	while vec.len() > 1 {
+		let mut ones_removed = vec![0usize; size];
+		let old_len = vec.len();
+		vec.retain(|num| {
+			let bin = u32::from_str_radix(&num, 2).unwrap();
+			let bit = (bin >> i) & 1;
+
+			let check = !co2 as u32;
+			let retain = bit == if len % 2 == 0 && counts[size - i - 1] == len / 2 { check } else if counts[size - i - 1] > len / 2 { check } else { 1 - check };
+			if !retain {
+				(0..size).for_each(|i| ones_removed[i] += ((bin >> (size - i - 1)) & 1) as usize);
+			}
+
+			retain
+		});
+
+		(0..size).for_each(|i| counts[i] -= ones_removed[i]);
+		len -= old_len - vec.len();
+		i = i.wrapping_sub(1);
+	}
+
+	u32::from_str_radix(&vec[0], 2).unwrap()
 }
 
 #[allow(dead_code)]
 pub fn day_03_part2(file: &str) -> u32 {
 	let vec = file_to_vec::<String>(file);
 
-	5
+	let size = vec[0].len();
+	let mut counts = vec![0usize; size];
+	for num in &vec {
+		let bin = u32::from_str_radix(&num, 2).unwrap();
+		(0..size).for_each(|i| counts[i] += ((bin >> (size - i - 1)) & 1) as usize);
+	}
+
+	let o2 = trim_rating(vec.clone(), size, counts.clone(), false);
+	let co2 = trim_rating(vec, size, counts, true);
+
+	o2 * co2
 }
 
 #[test]
 fn test_day_03() {
 	assert_eq!(day_03("day_03_test.txt"), 198);
-	assert_eq!(day_03_part2("day_o3_test.txt"), 230);
+	assert_eq!(day_03_part2("day_03_test.txt"), 230);
 }
--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,6 @@ mod day_03;
 fn main() {
 	//println!("{}", day_01::day_01("day_01.txt"));
 	//println!("{}", day_02::day_02("day_02.txt"));
-	println!("{}", day_03::day_03("day_03.txt"));
+	//println!("{}", day_03::day_03("day_03.txt"));
+	println!("{}", day_03::day_03_part2("day_03.txt"));
 }
